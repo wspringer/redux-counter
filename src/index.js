97d77f40f64abbdd0e320f0c195e2f41969baaa6
Wilfred Springer <wilfred@eastpole.nl>
2020-02-23 10:34:21 +0100

    Implement decrement *and* increment

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { connect, Provider } from 'react-redux';
 
 import './styles.scss';
 
-class Counter extends Component {
-  render() {
-    return (
-      <main className="Counter">
-        <p className="count">0</p>
-        <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
-        </section>
-      </main>
-    );
-  }
-}
+import { createStore } from 'redux';
 
-render(<Counter />, document.getElementById('root'));
+const counter = (state = 1, action) => {
+  switch (action.type) {
+    case 'INC': {
+      return state + 1;
+    }
+    case 'DECR': {
+      return state - 1;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const store = createStore(counter);
+
+const Counter = ({ count, increment, decrement }) => {
+  return (
+    <main className="Counter">
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button>Reset</button>
+      </section>
+    </main>
+  );
+};
+
+const mapStateToProps = state => ({
+  count: state,
+});
+
+const mapDispatchToProps = dispatch => ({
+  increment: () => dispatch({ type: 'INC' }),
+  decrement: () => dispatch({ type: 'DECR' }),
+});
+
+const CounterContainer = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+render(
+  <Provider store={store}>
+    <CounterContainer />
+  </Provider>,
+  document.getElementById('root'),
+);
